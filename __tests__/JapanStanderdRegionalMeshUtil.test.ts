1717b34e07327de2ab3ca40dd091d1921aa485ba
MasatoMakino <makino.masato.g@gmail.com>
2023-01-11 15:26:03 +0900

    refactor : test for toLatitudeLongitude

--- __tests__/JapanStanderdRegionalMeshUtil.test.ts
+++ __tests__/JapanStanderdRegionalMeshUtil.test.ts
@@ -7,64 +7,64 @@ describe("JapanStandardRegionalMeshUtil", () => {
   ); //東京タワー
   const testMeshCode = "53393599"; //東京タワーを含むメッシュコード
 
+  const testToLatLng = (
+    code: string,
+    codeLength: number,
+    expectLat: number,
+    expectLng: number
+  ) => {
+    const latLng = JapanStandardRegionalMeshUtil.toLatitudeLongitude(
+      code.slice(0, codeLength)
+    );
+    expect(latLng).toEqual(new LatitudeLongitude(expectLat, expectLng));
+  };
+
   test("Primary mesh code to lat-lng", () => {
-    const latLng = JapanStandardRegionalMeshUtil.toLatitudeLongitude(
-      testMeshCode.slice(0, 4)
-    );
-    expect(latLng).toEqual(new LatitudeLongitude(35.33333333333333, 139));
+    testToLatLng(testMeshCode, 4, 35.33333333333333, 139);
   });
-
   test("Secondary mesh code to lat-lng", () => {
-    const latLng = JapanStandardRegionalMeshUtil.toLatitudeLongitude(
-      testMeshCode.slice(0, 6)
-    );
-    expect(latLng).toEqual(new LatitudeLongitude(35.58333333333333, 139.625));
+    testToLatLng(testMeshCode, 6, 35.58333333333333, 139.625);
   });
-
   test("Standard mesh code to lat-lng", () => {
-    const latLng = JapanStandardRegionalMeshUtil.toLatitudeLongitude(
-      testMeshCode.slice(0, 8)
+    testToLatLng(testMeshCode, 8, 35.65833333333333, 139.7375);
+  });
+  test("Quad mesh 44 to lat-lng", () => {
+    testToLatLng(
+      testMeshCode + "44",
+      10,
+      35.66458333333333,
+      139.74687500000002
     );
-    expect(latLng).toEqual(new LatitudeLongitude(35.65833333333333, 139.7375));
   });
 
+  const testSubMesh = (subMeshCode: string) => {
+    expect(
+      JapanStandardRegionalMeshUtil.toLatitudeLongitude(testMeshCode)
+    ).toEqual(
+      JapanStandardRegionalMeshUtil.toLatitudeLongitude(
+        testMeshCode + subMeshCode
+      )
+    );
+  };
   test("Half mesh 1 === Standard mesh", () => {
-    expect(
-      JapanStandardRegionalMeshUtil.toLatitudeLongitude(testMeshCode)
-    ).toEqual(
-      JapanStandardRegionalMeshUtil.toLatitudeLongitude(testMeshCode + "1")
-    );
+    testSubMesh("1");
   });
-
   test("Quad mesh 11 === Standard mesh", () => {
-    expect(
-      JapanStandardRegionalMeshUtil.toLatitudeLongitude(testMeshCode)
-    ).toEqual(
-      JapanStandardRegionalMeshUtil.toLatitudeLongitude(testMeshCode + "11")
-    );
+    testSubMesh("11");
   });
 
-  test("Quad mesh 44", () => {
-    expect(
-      JapanStandardRegionalMeshUtil.toLatitudeLongitude(testMeshCode + "44")
-    ).toEqual(new LatitudeLongitude(35.66458333333333, 139.74687500000002));
-  });
-
-  test("NaN to lat-lng", () => {
-    const latLng = JapanStandardRegionalMeshUtil.toLatitudeLongitude("NaN");
+  const toBeUndefined = (code: string) => {
+    const latLng = JapanStandardRegionalMeshUtil.toLatitudeLongitude(code);
     expect(latLng).toEqual(undefined);
+  };
+  test("NaN to lat-lng are undefined", () => {
+    toBeUndefined("NaN");
   });
-
   test("Non-standard digits are undefined", () => {
-    expect(JapanStandardRegionalMeshUtil.toLatitudeLongitude("2")).toEqual(
-      undefined
-    );
-    expect(JapanStandardRegionalMeshUtil.toLatitudeLongitude("22222")).toEqual(
-      undefined
-    );
-    expect(
-      JapanStandardRegionalMeshUtil.toLatitudeLongitude("22222222222222")
-    ).toEqual(undefined);
+    toBeUndefined("");
+    toBeUndefined("2");
+    toBeUndefined("22222");
+    toBeUndefined("22222222222222");
   });
 
   test("fromLongitudeLatitude", () => {
