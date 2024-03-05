429500555186638c24291e1b2685e9e3f261f4a9
MasatoMakino <makino.masato.g@gmail.com>
2024-03-05 16:23:34 +0900

    add : unit test for CLI args

--- __tests__/CLI.spec.ts
+++ __tests__/CLI.spec.ts
@@ -5,7 +5,20 @@ import child_process from "child_process";
 const exec = util.promisify(child_process.exec);
 
 describe("CLI", () => {
-  it("should call CLI", async () => {
+  it("should correctly parse CLI arguments", async () => {
+    const meshCode = "53393599";
+    process.argv = [
+      "/usr/local/bin/node",
+      "./src/CLI.ts",
+      "generateTexture",
+      meshCode,
+    ];
+    const result = await import("../src/CLI.ts");
+
+    expect(result).toBeDefined();
+  });
+
+  it("should create a texture for the specified mesh code using CLI", async () => {
     const meshCode = "53393599";
     const { stdout, stderr } = await exec(
       `node ./esm/CLI.js generateTexture ${meshCode}`,
